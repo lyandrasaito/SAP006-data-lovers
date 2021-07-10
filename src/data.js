c51d15c43b33cb1df604aaa8e2c6a0c7c9660e8c
lyandrasaito <lyandrasaito@gmail.com>
2021-07-10 19:17:06 -0300

    feature/lya#7

--- src/data.js
+++ src/data.js
@@ -6,13 +6,13 @@ export const ordering = (element, option) => {
         return element.sort((a, b) => a.title > b.title ? 1 : -1);
     } else if (option === "Z-A") {
         return element.sort((a, b) => a.title > b.title ? -1 : -1);
-    } else if (option === "Release") {
+    } else if (option === "older") {
         return element.sort((a, b) => parseInt(a.release_date) > parseInt(b.release_date) ? 1 : -1);
-    } else if (option === "novo") {
+    } else if (option === "newer") {
         return element.sort((a, b) => parseInt(a.release_date) > parseInt(b.release_date) ? -1 : 1);
-    } else if (option === "Rate") {
+    } else if (option === "low") {
         return element.sort((a, b) => parseInt(a.rt_score) > parseInt(b.rt_score) ? 1 : -1);
-    } else if (option === "bom") {
+    } else if (option === "high") {
         return element.sort((a, b) => parseInt(a.rt_score) > parseInt(b.rt_score) ? -1 : 1);
     }
 }
