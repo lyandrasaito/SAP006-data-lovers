a57d5c133d2dffd14faab3701ab174985341226c
lyandrasaito <lyandrasaito@gmail.com>
2021-07-12 03:20:59 -0300

    Preparing to Filter

--- src/data.js
+++ src/data.js
@@ -19,3 +19,9 @@ export const ordering = (element, option) => {
     }
 }
 
+/*
+export const filtering(element, option) => {
+    if (option === ""){
+         
+    }
+}*/
--- src/main.js
+++ src/main.js
@@ -11,20 +11,15 @@ function print(ghibliMovies) {
 		card += `
 		<div class="card flexBox">
 			<div class="card-content">
-		
 				<h3 class="card-title">${property.title}</h3>
-		 
 				<div class="card-body">
-		
 					<h4>Director: ${property.director}</h4>
 					<h4>Producer: ${property.producer}</h4>
 					<h4>Release Date: ${property.release_date}</h4>
 					<h4>Score: ${property.rt_score}</h4>
 					<h4>Chronology of stories: ${property.chronological_position}º</h4>
 				</div>
-		
 			</div>
-		
 		</div>
 		`
 	}
@@ -39,11 +34,17 @@ function print(ghibliMovies) {
 }
 print(ghibliMovies);
 
-
 document.getElementById("order").addEventListener("change", (option) => {
 	const opt = option.target.value;
 	const order = ordering(ghibliMovies, opt);
 	print(order);
 });
+
+/*
+document.getElementById("filterDirector").addEventListener("change", (option) => {
+	const opt = option.target.value;
+	const filter = filtering(ghibliMovies, opt);
+	print(filter);
+});*/
 
 
