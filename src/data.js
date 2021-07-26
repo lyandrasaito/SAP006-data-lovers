6fb15cae9a24516ab872d13a38d4b096643aceec
lyandrasaito <lyandrasaito@gmail.com>
2021-07-26 04:15:51 -0300

    Refactoring functions. Adding spread operator. Tests covering 100% of statements.

--- src/data.js
+++ src/data.js
@@ -1,11 +1,9 @@
-//import data from './data/ghibli/ghibli.js'
-//const films = data.films;
-
-export const ordering = (element, option) => {
-    if (option === "A-Z") {
+export const ordering = (arr, option) => {
+    const element = [...arr];
+    if (option === "azMovies") {
         return element.sort((a, b) => a.title > b.title ? 1 : -1);
-    } else if (option === "Z-A") {
-        return element.sort((a, b) => a.title > b.title ? -1 : -1);
+    } else if (option === "zaMovies") {
+        return element.sort((a, b) => a.title > b.title ? -1 : 1);
     } else if (option === "older") {
         return element.sort((a, b) => parseInt(a.release_date) > parseInt(b.release_date) ? 1 : -1);
     } else if (option === "newer") {
@@ -16,31 +14,20 @@ export const ordering = (element, option) => {
         return element.sort((a, b) => parseInt(a.rt_score) > parseInt(b.rt_score) ? -1 : 1);
     } else if (option === "internal") {
         return element.sort((a, b) => parseInt(a.chronological_position) > parseInt(b.chronological_position) ? 1 : -1);
-    }
-}
-
-export const orderingCharacters = (element, option) => {
-    if (option === "A-Z") {
+    } else if (option === "azCharacters") {
         return element.sort((a, b) => a.name > b.name ? 1 : -1);
-    } else if (option === "Z-A") {
+    } else if (option === "zaCharacters") {
         return element.sort((a, b) => a.name > b.name ? -1 : 1);
     }
 }
 
-export const filteringDirector = (element, option) => {
-    return element.filter((e) => e.director === option)
-}
+export const filteringDirector = (element, option) => element.filter((e) => e.director === option);
 
-export const filteringProducer = (element, option) => {
-    return element.filter((e) => e.producer === option)
-}
+export const filteringProducer = (element, option) => element.filter((e) => e.producer === option);
 
-export const filteringGender = (element, option) => {
-    return element.filter((e) => e.gender === option)
-}
+export const filteringGender = (element, option) => element.filter((e) => e.gender.toLowerCase() === option);
 
-export const filteringSpecies = (element, option) => {
-    return element.filter((e) => e.specie === option)
-}
+export const filteringSpecies = (element, option) => element.filter((e) => e.specie.toLowerCase() === option);
+
 
 
